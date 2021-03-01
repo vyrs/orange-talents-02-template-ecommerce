586493ae977d931ae515e4b07643c3905e885515
vyrs <vitory.rsilva@hotmail.com>
2021-03-01 11:36:26 -0300

    adicionando classe de validação

---
+++ src/main/java/br/com/zup/mercadolivre/compartilhado/ErroDeFormularioRequest.java
@@ -0,0 +1,19 @@
+package br.com.zup.mercadolivre.compartilhado;
+
+public class ErroDeFormularioRequest {
+    private String campo;
+    private String erro;
+
+    public ErroDeFormularioRequest(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+}
